1927292d27441a10c878c2c115f0e953d89ac5f9
Clément Tamisier <clement.tamisier@gmail.com>
2021-04-28 12:59:30 +0200

    fix: upgrade date-fns

--- generators/client/templates/quasar/src/util/format.js
+++ generators/client/templates/quasar/src/util/format.js
@@ -2,9 +2,9 @@ import { format as format_, formatDistanceStrict as formatDistanceStrict_ } from
 import { importLocale } from '../constants/i18nConstants';
 
 export function format(date, formatStr = 'PPpp') {
-  return format_(date, formatStr, { locale: importLocale() });
+  return format_(date, formatStr, { locale: importLocale().default });
 }
 
 export function formatDistanceStrict(date, baseDate = new Date(0)) {
-  return formatDistanceStrict_(date, baseDate, { locale: importLocale() });
+  return formatDistanceStrict_(date, baseDate, { locale: importLocale().default });
 }
