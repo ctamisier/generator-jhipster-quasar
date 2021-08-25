c997134983fc47894752f82da8318d8996f8e046
Avdev4J <37835668+avdev4j@users.noreply.github.com>
2021-08-25 11:12:43 +0200

    Don't prompt value if we have only one (#4)

--- generators/client/prompts.js
+++ generators/client/prompts.js
@@ -2,32 +2,9 @@ module.exports = {
     askForClient,
 };
 
-function askForClient(meta) {
+async function askForClient(meta) {
     if (!meta && this.existingProject) return;
-    const applicationType = this.applicationType;
 
-    const choices = [
-        {
-            value: 'quasar',
-            name: 'Quasar',
-        },
-    ];
-
-    const PROMPT = {
-        type: 'list',
-        name: 'clientFramework',
-        when: () => applicationType !== 'microservice' && applicationType !== 'uaa',
-        message: () => 'Which *Framework* would you like to use for the client?',
-        choices,
-        default: 'quasar',
-    };
-
-    if (meta) return PROMPT; // eslint-disable-line consistent-return
-
-    const done = this.async();
-
-    this.prompt(PROMPT).then(prompt => {
-        this.clientFramework = prompt.clientFramework;
-        done();
-    });
+    this.info('Client framework automatically set to Quasar');
+    this.clientFramework = this.jhipsterConfig.clientFramework = 'quasar';
 }
