4e9db3089691baa680a9fd2830367fd58f33ce74
Anthony Viard <anth.viard@gmail.com>
2021-08-25 10:58:53 +0200

    Don't prompt value if we have only one

--- generators/app/prompts.js
+++ generators/app/prompts.js
@@ -1,5 +1,3 @@
-const chalk = require('chalk');
-
 module.exports = {
     askForApplicationType,
 };
@@ -7,20 +5,6 @@ module.exports = {
 async function askForApplicationType() {
     if (this.existingProject) return;
 
-    const answers = await this.prompt([
-        {
-            type: 'list',
-            name: 'applicationType',
-            message: `Which ${chalk.yellow('*type*')} of application would you like to create?`,
-            choices: [
-                {
-                    value: 'monolith',
-                    name: 'Monolithic application',
-                },
-            ],
-            default: 'monolith',
-        },
-    ]);
-
-    this.applicationType = this.jhipsterConfig.applicationType = answers.applicationType;
+    this.info('Application type forced to Monolith');
+    this.applicationType = this.jhipsterConfig.applicationType = 'monolith';
 }
